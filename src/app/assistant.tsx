03ae8c886764623e7d0fa1b1b96e176dc9068e87
Lennex Zinyando <brizdigital@gmail.com>
2025-03-27 17:08:28 +0200

    Assistant UI initial setup

---
+++ src/app/assistant.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { AssistantRuntimeProvider } from "@assistant-ui/react";
+import { useChatRuntime } from "@assistant-ui/react-ai-sdk";
+import { Thread } from "@/components/assistant-ui/thread";
+
+export const Assistant = () => {
+  const runtime = useChatRuntime({
+    api: "/api/chat",
+  });
+
+  return (
+    <AssistantRuntimeProvider runtime={runtime}>
+      <div className="grid h-dvh gap-x-2 px-4 py-4">
+        <Thread />
+      </div>
+    </AssistantRuntimeProvider>
+  );
+};
