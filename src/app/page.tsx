8a48ae572713c757e1f3636ed72e33af322f5817
Lennex Zinyando <brizdigital@gmail.com>
2025-03-31 15:10:13 +0200

    Cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-// src/app/page.tsx
-"use client";
-
 import { Assistant } from "./assistant";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center w-full min-h-screen p-4">
-      <div className="flex w-full max-w-7xl gap-6">
-        <div className="flex-1 min-w-[500px]">
-          <Assistant />
-        </div>
-      </div>
+    <main>
+      <Assistant />
     </main>
   );
 }
